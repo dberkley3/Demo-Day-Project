94fc35e3327e39100cf17176dadaace031ca1f66
dberkley3 <dberkley3@gmail.com>
2022-06-06 09:32:56 -0400

    second commit

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = { //exporting an object
+
+    'url' : 'mongodb+srv://demo:day@cluster0.smrsm.mongodb.net/?retryWrites=true&w=majority', 
+    'dbName': 'demo day app'
+};
